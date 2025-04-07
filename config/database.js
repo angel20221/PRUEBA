a5a471052cde29a0c2196a59c87ae5defd466df2
Angel Machicado <angelmachicado40@gmail.com>
2025-04-07 09:41:12 -0400

    Primer commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2/promise');
 
-// Configuración para Clever Cloud (usa sus variables automáticas)
 const pool = mysql.createPool({
   host: process.env.MYSQL_ADDON_HOST || 'localhost',
   user: process.env.MYSQL_ADDON_USER || 'root',
   password: process.env.MYSQL_ADDON_PASSWORD || '',
   database: process.env.MYSQL_ADDON_DB || 'bd_biblioteca',
-  port: process.env.MYSQL_ADDON_PORT || 3306, // Clever usa 3366, local 3306
+  port: process.env.MYSQL_ADDON_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  ssl: process.env.MYSQL_ADDON_HOST ? { rejectUnauthorized: false } : null
+  ssl: process.env.MYSQL_ADDON_HOST ? { 
+    rejectUnauthorized: true, 
+    ca: process.env.MYSQL_ADDON_CA 
+  } : null
 });
 
 module.exports = pool;
